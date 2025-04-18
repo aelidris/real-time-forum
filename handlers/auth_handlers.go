e45f658efc169c89a5292e46ded8e62fe8ff7dd1
aelidris <smias1.1998@gmail.com>
2025-04-18 10:03:33 +0100

    24 hours for session expired and -1 if you need to expired immediately like in logout

--- handlers/auth_handlers.go
+++ handlers/auth_handlers.go
@@ -95,7 +95,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    "session_token",
 		Value:   sessionToken,
-		Expires: time.Now().Add(1 * time.Hour),
+		Expires: time.Now().Add(24 * time.Hour),
 	})
 
 	response := map[string]string{
@@ -248,7 +248,7 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    "session_token",
 		Value:   "guest",
-		Expires: time.Now().Add(1 * time.Hour),
+		Expires: time.Now().Add(-1 * time.Hour),
 	})
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 	fmt.Fprintln(w, "You have been logged out.")
